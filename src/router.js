5cea170bb9111e20b428f18555098dcf938aed8e
rodrigo <digo3007@gmail.com>
2023-07-31 17:40:14 -0300

    update router file

--- src/router.js
+++ src/router.js
@@ -1,27 +1,26 @@
-export class Router  {
+export class Router {
+  routes = {}; //somente para identificar como obj
 
-    routes = {} //somente para identificar como obj
+  add(routeName, page) {
+    this.routes[this.routeName] = page;
+  }
 
-    add(routeName, page) {
-        this.routes[this.routeName] = page;
-    }
+  route(event) {
+    event = event || window.event;
+    event.preventDefault();
 
-    route(event) {
-        event = event || window.event;
-        event.preventDefault();
-        
-        window.history.pushState({}, "", event.target.href);
-        
-        this.handle();
-        }
-        
-    handle() {
-        const { pathname } = window.location;
-        const route = this.routes[pathname] || this.routes[404];
-        fetch(route)
-            .then((data) => data.text())
-            .then((html) => {
-            document.querySelector("#app").innerHTML = html;
-            }); 
-        }
+    window.history.pushState({}, "", event.target.href);
+
+    this.handle();
+  }
+
+  handle() {
+    const { pathname } = window.location;
+    const route = this.routes[pathname] || this.routes[404];
+    fetch(route)
+      .then((data) => data.text())
+      .then((html) => {
+        document.querySelector("#app").innerHTML = html;
+      });
+  }
 }
